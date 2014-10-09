bfd569e877f3fbe19de450ae0815eaf4541452fa
philhofer <phofer@umich.edu>
2014-10-09 17:16:12 -0400

    raw []byte

---
+++ enc/raw.go
@@ -0,0 +1,18 @@
+package enc
+
+import (
+	"io"
+)
+
+// Raw is a []byte that is already messagepack-formatted
+type Raw []byte
+
+// Raw implements the MsgEncoder interface
+func (r Raw) EncodeMsg(w io.Writer) (int, error) {
+	return w.Write([]byte(r))
+}
+
+// Raw implements the MsgEncoder interface
+func (r Raw) EncodeTo(wr MsgWriter) (int, error) {
+	return wr.Write([]byte(r))
+}
