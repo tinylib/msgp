d484a812023189e2871949b1c1dc9bfd028873ed
Peter Waller <p@pwaller.net>
2016-07-15 15:07:14 +0100

    Add comment to generated Msgsize function
    This avoids the error message raised by golint:
    
    > .Msgsize should have comment or be unexported
    
    https://github.com/golang/lint/blob/c7bacac2b21ca01afa1dee0acf64df3ce047c28f/lint.go#L777

--- gen/size.go
+++ gen/size.go
@@ -79,6 +79,8 @@ func (s *sizeGen) Execute(p Elem) error {
 		return nil
 	}
 
+	s.p.comment("Msgsize returns the number of bytes occupied by the serialized message")
+
 	s.p.printf("\nfunc (%s %s) Msgsize() (s int) {", p.Varname(), imutMethodReceiver(p))
 	s.state = assign
 	next(s, p)
