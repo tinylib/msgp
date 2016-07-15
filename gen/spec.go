6da8d3d71e6e96697d5a2e013e1c85355faf43ab
s.kamardin <s.kamardin@corp.mail.ru>
2016-07-15 12:48:52 +0300

    declare vars out of the loop

--- gen/spec.go
+++ gen/spec.go
@@ -9,8 +9,6 @@ import (
 )
 
 const (
-	field       = "field"
-	typ         = "typ"
 	errcheck    = "\nif err != nil { return }"
 	lenAsUint32 = "uint32(len(%s))"
 	literalFmt  = "%s"
@@ -235,8 +233,8 @@ func genStructFieldsSerializer(t traversalFuser, p printer, fields []StructField
 
 		data, p.err = msgp.AppendIntf(nil, f.FieldTag)
 		p.printf(
-			"\n// write struct field key %q = %v; declared as type %T, eventually become msgp.%s",
-			f.FieldName, f.FieldTag, f.FieldTag, msgp.NextType(data),
+			"\n// write label for the struct field %q: %v typed as msgp.%s",
+			f.FieldName, f.FieldTag, msgp.NextType(data),
 		)
 		t.Fuse(data)
 
@@ -245,63 +243,74 @@ func genStructFieldsSerializer(t traversalFuser, p printer, fields []StructField
 }
 
 func genStructFieldsParser(t traversalAssigner, p printer, fields []StructField) {
-	sz := randIdent()
-	p.declare(sz, u32)
-	t.assignAndCheck(sz, mapHeader)
-
-	p.printf("\nfor %s > 0 {\n%s--", sz, sz)
+	const (
+		fieldBytes = "field_b"
+		fieldInt   = "field_i"
+		fieldUint  = "field_u"
+		typ        = "typ"
+	)
 
 	groups := groupFieldsByType(fields)
 	hasUint := len(groups[msgp.UintType]) > 0
 	hasInt := len(groups[msgp.IntType]) > 0
 	hasStr := len(groups[msgp.StrType]) > 0
 
-	if !hasUint && !hasInt { // there are no numerically labeled fields, so parse every field label as string
-		p.declare(field, "[]byte")
-		t.assignAndCheck(field, mapKey)
-		switchFieldKeysStr(t, p, fields)
+	if hasStr {
+		p.declare(fieldBytes, "[]byte")
+	}
+	if hasUint {
+		p.declare(fieldUint, "uint64")
+
+		// Append to int fields also uint fields that do not overflow int64.
+		// This is necessary because uint field with value <= (1<<7)-1 could be serialized as fixint.
+		// and become a msgp.IntType. This is done for best compatibility with other libraries
+		// (and with other languages). That is, some endpoint could serialize uint16 key with value <= (1<<7)-1 as
+		// real msgpack uint16, but also could serialize it like fixint.
+		for _, f := range groups[msgp.UintType] {
+			v := f.FieldTag.(uint64)
+			if v <= math.MaxInt64 {
+				groups[msgp.IntType] = append(groups[msgp.IntType], f)
+				hasInt = true
+			}
+		}
+	}
+	if hasInt {
+		p.declare(fieldInt, "int64")
+	}
+	if hasInt || hasUint {
+		p.declare(typ, "msgp.Type")
+	}
+
+	sz := randIdent()
+	p.declare(sz, u32)
+	t.assignAndCheck(sz, mapHeader)
+	p.printf("\nfor %s > 0 {\n%s--", sz, sz)
+
+	if hasStr && !hasUint && !hasInt { // there are no numerically labeled fields, so parse every field label as string
+		t.assignAndCheck(fieldBytes, mapKey)
+		switchFieldKeysStr(t, p, fields, fieldBytes)
 	} else {
 		// switch on inferred type of next field
-		p.declare(typ, "msgp.Type")
 		t.nextTypeAndCheck(typ)
 		p.printf("\nswitch %s {", typ)
-
 		if hasUint {
 			p.print("\ncase msgp.UintType:")
-			p.declare(field, "uint64")
-			t.assignAndCheck(field, "Uint64")
-			switchFieldKeys(t, p, groups[msgp.UintType])
-
-			// Append to int fields also uint fields that do not overflow int64.
-			// This is necessary because uint field with value <= (1<<7)-1 could be serialized as fixint.
-			// and become a msgp.IntType. This is done for best compatibility with other libraries
-			// (and with other languages). That is, some endpoint could serialize uint16 key with value <= (1<<7)-1 as
-			// real msgpack uint16, but also could serialize it like fixint.
-			for _, f := range groups[msgp.UintType] {
-				v := f.FieldTag.(uint64)
-				if v <= math.MaxInt64 {
-					groups[msgp.IntType] = append(groups[msgp.IntType], f)
-					hasInt = true
-				}
-			}
+			t.assignAndCheck(fieldUint, "Uint64")
+			switchFieldKeys(t, p, groups[msgp.UintType], fieldUint)
 		}
 		if hasInt {
 			p.print("\ncase msgp.IntType:")
-			p.declare(field, "int64")
-			t.assignAndCheck(field, "Int64")
-			switchFieldKeys(t, p, groups[msgp.IntType])
+			t.assignAndCheck(fieldInt, "Int64")
+			switchFieldKeys(t, p, groups[msgp.IntType], fieldInt)
 		}
 		if hasStr {
 			// double case is done for backward compatibility with previous implementation
 			p.print("\ncase msgp.StrType, msgp.BinType:")
-			p.declare(field, "[]byte")
-			t.assignAndCheck(field, mapKey)
-			switchFieldKeysStr(t, p, groups[msgp.StrType])
+			t.assignAndCheck(fieldBytes, mapKey)
+			switchFieldKeysStr(t, p, groups[msgp.StrType], fieldBytes)
 		}
-
 		p.print("\ndefault:")
 		t.skipAndCheck()
-
 		p.closeblock() // close switch
 	}
 
@@ -327,8 +336,8 @@ func groupFieldsByType(fields []StructField) map[msgp.Type][]StructField {
 	return groups
 }
 
-func switchFieldKeysStr(t traversalAssigner, p printer, fields []StructField) {
-	p.printf("\nswitch msgp.UnsafeString(%s) {", field)
+func switchFieldKeysStr(t traversalAssigner, p printer, fields []StructField, label string) {
+	p.printf("\nswitch msgp.UnsafeString(%s) {", label)
 	for _, f := range fields {
 		p.printf("\ncase \"%s\":", f.FieldTag)
 		next(t, f.FieldElem)
@@ -343,8 +352,8 @@ func switchFieldKeysStr(t traversalAssigner, p printer, fields []StructField) {
 	p.closeblock() // close switch
 }
 
-func switchFieldKeys(t traversalAssigner, p printer, fields []StructField) {
-	p.printf("\nswitch %s {", field)
+func switchFieldKeys(t traversalAssigner, p printer, fields []StructField, label string) {
+	p.printf("\nswitch %s {", label)
 	for _, f := range fields {
 		p.printf("\ncase %v:", f.FieldTag)
 		next(t, f.FieldElem)
