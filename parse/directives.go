801e332855e5d0ef7f9dbf21c563851dae07d10e
very-amused <very-amused@pm.me>
2024-05-18 18:58:02 -0400

    Implement custom tag support via msgp:tag directive

--- parse/directives.go
+++ parse/directives.go
@@ -24,7 +24,15 @@ var directives = map[string]directive{
 	"shim":   applyShim,
 	"ignore": ignore,
 	"tuple":  astuple,
-}
+	"tag":    tag}
+
+// map of all recognized directives which will be applied
+// before process() is called
+//
+// to add an early directive, define a func([]string, *FileSet) error
+// and then add it to this list.
+var earlyDirectives = map[string]directive{
+	"tag": tag}
 
 var passDirectives = map[string]passDirective{
 	"ignore": passignore,
@@ -126,5 +134,14 @@ func astuple(text []string, f *FileSet) error {
 			}
 		}
 	}
+	return nil
+}
+
+//msgp:tag {tagname}
+func tag(text []string, f *FileSet) error {
+	if len(text) != 2 {
+		return nil
+	}
+	f.tagName = strings.TrimSpace(text[1])
 	return nil
 }
